8b81c0ad927db73f76bb5bac4af3cc8717999703
Tejas-08 <tejasgowda07177@gmail.com>
2026-03-05 12:44:05 +0100

    Exercise 1: added pipeline stage instances

--- rtl/cpu.sv
+++ rtl/cpu.sv
@@ -17,4 +17,30 @@ module cpu (
     input logic external_interrupt_in,
     input logic timer_interrupt_in
 );
+fetch_stage fetch(
+    .clk(clk),
+    .rst(rst),
+    .memory_fetch_port(memory_fetch_port)
+);
+
+decode_stage decode(
+    .clk(clk),
+    .rst(rst)
+);
+
+execute_stage execute(
+    .clk(clk),
+    .rst(rst)
+);
+
+memory_stage memory(
+    .clk(clk),
+    .rst(rst),
+    .memory_mem_port(memory_mem_port)
+);
+
+writeback_stage writeback(
+    .clk(clk),
+    .rst(rst)
+);
 endmodule

a4310eb79385b0abd8034cec259b55e01a37ae2a
Tejas-08 <tejasgowda07177@gmail.com>
2026-03-16 14:16:19 +0100

    Exercise 2: implement fetch stage

--- rtl/fetch_stage.sv
+++ rtl/fetch_stage.sv
@@ -1,30 +1,34 @@
-/* Copyright (c) 2024 Tobias Scheipel, David Beikircher, Florian Riedl
- * Embedded Architectures & Systems Group, Graz University of Technology
- * SPDX-License-Identifier: MIT
- * ---------------------------------------------------------------------
- * File: fetch_stage.sv
- */
-
-
-
 module fetch_stage (
     input logic clk,
     input logic rst,
 
-    // Memory interface
     wishbone_interface.master wb,
 
-    //  Output data
     output logic [31:0] instruction_reg_out,
     output logic [31:0] program_counter_reg_out,
 
-    // Pipeline control
-    output pipeline_status::forwards_t  status_forwards_out,
-    input  pipeline_status::backwards_t status_backwards_in,
-    input  logic [31:0] jump_address_backwards_in
+    output pipeline_status::forwards_t status_forwards_out,
+    input pipeline_status::backwards_t status_backwards_in,
+    input logic [31:0] jump_address_backwards_in
 );
 
-    // TODO: Delete the following line and implement this module.
-    ref_fetch_stage golden(.*);
+logic [31:0] pc;
+
+always_ff @(posedge clk or posedge rst) begin
+    if (rst)
+        pc <= 32'b0;
+    else
+        pc <= pc + 4;
+end
+
+assign wb.adr = pc;
+assign wb.cyc = 1'b1;
+assign wb.stb = 1'b1;
+assign wb.we  = 1'b0;
+
+always_ff @(posedge clk) begin
+    instruction_reg_out <= wb.dat_r;
+    program_counter_reg_out <= pc;
+end
 
 endmodule
